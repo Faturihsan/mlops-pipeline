9fdb20c668fc882a3701a593021097b8f4ae1d37
faturihsann <faturihsan.dev@gmail.com>
2025-07-15 11:55:01 +0800

    testing sync

--- scripts/sync_to_notebook.sh
+++ scripts/sync_to_notebook.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Define your notebook pod (adjust the label if needed)
-POD_NAME=$(kubectl get pod -n kubeflow -l app=jupyter -o jsonpath="{.items[0].metadata.name}")
+POD_NAME=$(kubectl get pod -n kubeflow-user-example-com -o name | grep object-detection)
 
 # Exec into notebook pod and pull latest code
 kubectl exec -n kubeflow "$POD_NAME" -- bash -c "
 if [ ! -d /home/jovyan/mlops-project ]; then
-  cd /home/jovyan && git clone https://gitlab.com/<your-user>/<your-repo>.git mlops-project
+  cd /home/jovyan && git clone https://gitlab.com/faturihsan-skripsi/mlops-pipeline-automation.git mlops-pipeline-automation
 else
   cd /home/jovyan/mlops-project && git pull origin main
 fi
